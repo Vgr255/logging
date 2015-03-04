17f2ca5ea2c5de1df7eebf4709d567e12d0436a5
Vgr E.Barry <vgr255@live.ca>
2015-03-03 19:30:38 -0500

    Improve Bypassers.copy to do a true deep copy instead of a shallow one.

--- logger.py
+++ logger.py
@@ -399,7 +399,7 @@ class Bypassers(Container):
         """Return a new instance with the same attributes."""
         new = []
         for setting, ((types, pairs), module, attr) in self._items.items():
-            new.append((setting, types, pairs, module, attr))
+            new.append((setting, set(types), set(pairs), module, attr))
         return self.__class__(*new)
 
     def clear(self):
