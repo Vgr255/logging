f4f067091923c26780865a78dc3e044de15dd048
Vgr E.Barry <vgr255@live.ca>
2015-03-09 18:44:47 -0400

    Remove superfluous space.

--- logger.py
+++ logger.py
@@ -12,7 +12,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Improved Logger module by Vgr v0.1
 Documentation string still to-do."""
