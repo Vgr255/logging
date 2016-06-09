fa19a6ec882727bb96f27993d7ac765797c19556
Vgr E. Barry <vgr255@live.ca>
2016-06-08 22:22:00 -0400

    Add a find_name utility function

--- logger/utilities.py
+++ logger/utilities.py
@@ -2,7 +2,9 @@
 
 """Small utility functions for use in various places."""
 
-__all__ = ["pick", "is_dunder"]
+__all__ = ["pick", "is_dunder", "find_name"]
+
+import sys
 
 def pick(arg, default):
     """Handler for default versus given argument."""
@@ -11,3 +13,15 @@ def pick(arg, default):
 def is_dunder(name):
     """Return True if a __dunder__ name, False otherwise."""
     return name[:2] == name[-2:] == "__" and "_" not in (name[2:3], name[-3:-2])
+
+def find_name(name, depth=0):
+    """Find a name in the calling frame's scopes."""
+    calling_frame = sys._getframe(depth + 2)
+    if name in calling_frame.f_locals:
+        return calling_frame.f_locals[name]
+    if name in calling_frame.f_globals:
+        return calling_frame.f_globals[name]
+    if name in calling_frame.f_builtins:
+        return calling_frame.f_builtins[name]
+
+    raise NameError("could not find {!r}".format(name))
