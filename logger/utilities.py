ac2160d1ab84ee700aab1a9e5a9df8c458bebe55
Vgr E. Barry <vgr255@live.ca>
2016-06-10 22:45:14 -0400

    Remove the sorter function
    It was ugly, very bad, and not even used anymore since the beginning of
    the refactor.

--- logger/utilities.py
+++ logger/utilities.py
@@ -17,15 +17,3 @@ def is_dunder(name):
 def convert_to_od(mapping, order):
     """Convert mapping to an OrderedDict instance using order."""
     return collections.OrderedDict([(i, mapping[i]) for i in order])
-
-def sorter(x):
-    """Make sure non-string objects are last."""
-    # this is very ugly and I am not pleased with it, so if anyone can
-    # come up with a better solution, I'm all open
-    # see http://bugs.python.org/issue20630 and
-    #     http://bugs.python.org/issue20632
-    if "__lt__" not in type(x).__dict__:
-        return "????????"
-    if not isinstance(x, str):
-        return "????" + str(x).lower()
-    return x.lower()
