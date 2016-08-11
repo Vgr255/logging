ae317ab3d801d94eb4f264da55e420c2bf959396
Vgr E.Barry <vgr255@live.ca>
2016-08-11 11:36:10 -0400

    Add new io submodule
    Not even started yet, but the module is there. There is no plan to
    remain compatible with Python's own I/O machinery, instead a more
    versatile and practical approach is envisioned.

---
+++ logger/io.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Specific module for I/O-related operations."""
+
+__all__ = []
+
+import stat
+import os
+
+class IOBase:
+    """Base class for I/O-related operations."""
